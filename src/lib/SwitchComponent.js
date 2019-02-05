0f52a24e75a3b8172c7074c9974780cf9fc834db
Colin Carter <colin.carter@axsy.com>
2019-02-05 10:37:04 +0000

    Use ViewPropsTypes as Switch propTypes no longer exist

--- src/lib/SwitchComponent.js
+++ src/lib/SwitchComponent.js
@@ -56,5 +56,5 @@ export class SwitchComponent extends React.Component{
 SwitchComponent.propTypes = {
     labelStyle: Text.propTypes.style,
     containerStyle: ViewPropTypes.style,
-    switchStyle: Switch.propTypes.style
+    switchStyle: ViewPropTypes.style
 }
