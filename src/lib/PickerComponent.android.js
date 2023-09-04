ea05a3a1a428a6da7ca1739702881e1a3d72b094
scottpage1317 <scott.page@axsy.com>
2023-09-04 18:25:21 +0100

    remove incorrect / unneeded touchable prop from android specific code

--- src/lib/PickerComponent.android.js
+++ src/lib/PickerComponent.android.js
@@ -72,7 +72,7 @@ export class PickerComponent extends React.Component {
                   this
                 )}
               </Picker>
-              <TouchableOpacity activeOpacity={0} style={pickerCoverStyle} touchable={false} />
+              <TouchableOpacity activeOpacity={0} style={pickerCoverStyle} />
             </View>
           </View>
         </Field>
